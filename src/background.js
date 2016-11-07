ec90a1c3660f0507a4120187281c9a7e4222e941
xtangle <jackyx5724@hotmail.com>
2016-11-06 23:11:01 -0500

    fix issue where jQuery is not loaded before content script

--- src/background.js
+++ src/background.js
@@ -38,16 +38,18 @@ chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
   if (details.frameId === 0) {
     chrome.tabs.get(details.tabId, function (tab) {
       if (tab.url === details.url) {
-        chrome.tabs.executeScript(null, {file: "src/content.js"});
+        chrome.tabs.executeScript(null, {file: 'lib/jquery-3.1.1.min.js'}, function() {
+          chrome.tabs.executeScript(null, {file: 'src/content.js'});
+        });
       }
     });
   }
 });
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (tab.url.indexOf("*://soundcloud.com/*/sets/*") > -1 &&
+  if (tab.url.indexOf('*://soundcloud.com/*/sets/*') > -1 &&
     changeInfo.url === undefined) {
-    chrome.tabs.executeScript(tabId, {file: "src/content.js"});
+    chrome.tabs.executeScript(tabId, {file: 'src/content.js'});
   }
 });
 
