3632ccaf0dc755dfc647b07c718793fc95d6c3a5
xtangle <jackyx5724@hotmail.com>
2017-01-15 23:47:44 -0500

    JS - Refactor gulpfile

--- gulp-tasks/build.js
+++ gulp-tasks/build.js
@@ -1,34 +1,23 @@
 const gulp = require('gulp');
 const fs = require('fs');
 const path = require('path');
-const del = require('del');
 const crx = require('gulp-crx-pack');
-
+const del = require('del');
+const pump = require('pump');
 const conf = require('../config/gulp.config.json');
-const manifest = require(path.resolve(conf.files.manifest));
-const key = fs.readFileSync(path.resolve(conf.files.key), 'utf8');
 
-gulp.task('clean', gulp.parallel(cleanBuild, cleanDist));
 gulp.task('clean:build', cleanBuild);
 gulp.task('clean:dist', cleanDist);
+gulp.task('clean', gulp.parallel('clean:build', 'clean:dist'));
 
-gulp.task('build', gulp.series(cleanBuild, build));
-gulp.task('build:dist', gulp.series(gulp.parallel(build, cleanDist), buildDist));
+gulp.task('copy', copyAssets);
+gulp.task('build', gulp.series(
+  'clean:build', gulp.parallel('copy', buildJavascript)
+));
+gulp.task('build:dist', gulp.series(
+  gulp.parallel('build', 'clean:dist'), buildExtension
+));
 gulp.task('dist', gulp.series('build:dist'));
-
-function build() {
-  return gulp.src(path.join(path.resolve(conf.paths.src), '**', '*'))
-    .pipe(gulp.dest(path.resolve(conf.paths.build)));
-}
-
-function buildDist() {
-  return gulp.src(path.resolve(conf.paths.build))
-    .pipe(crx({
-      privateKey: key,
-      filename: manifest.name + '.crx',
-    }))
-    .pipe(gulp.dest(path.resolve(conf.paths.dist)));
-}
 
 function cleanBuild() {
   return del(path.resolve(conf.paths.build));
@@ -36,4 +25,40 @@ function cleanBuild() {
 
 function cleanDist() {
   return del(path.resolve(conf.paths.dist));
+}
+
+function copyAssets(cb) {
+  const src = conf.paths.src;
+  pump([
+    gulp.src([
+      `${src}/**/*`,
+      `!${src}/{js, js/**/*}`,
+    ]),
+    gulp.dest(path.resolve(conf.paths.build)),
+  ], cb);
+}
+
+function buildJavascript(cb) {
+  const js = `${conf.paths.src}/js`;
+  pump([
+    gulp.src([
+      `${js}/**/*`,
+      `!${js}/{common, common/**/*}`,
+      `!${js}/{lib, lib/**/*}`,
+    ]),
+    gulp.dest(path.join(path.resolve(conf.paths.build), 'js')),
+  ], cb);
+}
+
+function buildExtension(cb) {
+  const manifest = require(path.resolve(conf.files.manifest));
+  const key = fs.readFileSync(path.resolve(conf.files.key), 'utf8');
+  pump([
+    gulp.src(path.resolve(conf.paths.build)),
+    crx({
+      privateKey: key,
+      filename: manifest.name + '.crx',
+    }),
+    gulp.dest(path.resolve(conf.paths.dist)),
+  ], cb);
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,8 @@
 const gulp = require('gulp');
-const path = require('path');
 const HubRegistry = require('gulp-hub');
 const conf = require('./config/gulp.config.json');
 
-let hub = new HubRegistry(path.join(conf.paths.tasks, '**', '*.js'));
-
+let hub = new HubRegistry(`${conf.paths.tasks}/**/*.js`);
 gulp.registry(hub);
 
 gulp.task('default', gulp.series('build:dist'));
