072d2846acd3c142db82873d55fa6bec76fdf9e4
xtangle <jackyx5724@hotmail.com>
2018-06-13 01:52:03 -0400

    test(download): add tests for playlist and user download services

--- test/ts/download/playlist-download-service.spec.ts
+++ test/ts/download/playlist-download-service.spec.ts
@@ -1,21 +1,19 @@
-/*
-import {IPlaylistInfo, ITrackInfo, IUserInfo} from '@src/download/download-info';
+import {ITrackDownloadResult} from '@src/download/download-result';
 import {PlaylistDownloadService} from '@src/download/playlist-download-service';
+import {IPlaylistInfo, ITrackInfo, IUserInfo, ResourceType} from '@src/download/resource-info';
 import {TrackDownloadService} from '@src/download/track-download-service';
-import {useRxTesting, useSinonChai} from '@test/test-initializers';
-import {of, throwError} from 'rxjs';
+import {useSinonChai} from '@test/test-initializers';
 import {match, SinonStub, stub} from 'sinon';
 
 const expect = useSinonChai();
 
 describe('playlist download service', () => {
-  const rx = useRxTesting();
   const fixture = PlaylistDownloadService;
 
   const trackOneInfo: ITrackInfo = {title: 'foo'} as ITrackInfo;
   const trackTwoInfo: ITrackInfo = {title: 'bar'} as ITrackInfo;
   const playlistInfo: IPlaylistInfo = {
-    kind: 'playlist',
+    kind: ResourceType.Playlist,
     permalink_url: 'permalinkUrl',
     title: 'some*playlist?with>special\\characters',
     tracks: [trackOneInfo, trackTwoInfo],
@@ -23,53 +21,47 @@ describe('playlist download service', () => {
       username: 'some<username|with?special/characters'
     } as IUserInfo
   };
+  const trackOneDlResult = {trackInfo: trackOneInfo} as ITrackDownloadResult;
+  const trackTwoDlResult = {trackInfo: trackTwoInfo} as ITrackDownloadResult;
 
   describe('downloading a playlist', () => {
-    let stubDownloadTrack$: SinonStub;
+    let stubDownloadTrack: SinonStub;
 
     beforeEach(() => {
-      stubDownloadTrack$ = stub(TrackDownloadService, 'download$');
-      stubDownloadTrack$.withArgs(trackOneInfo, match.any).returns(of(1));
-      stubDownloadTrack$.withArgs(trackTwoInfo, match.any).returns(of(2));
+      stubDownloadTrack = stub(TrackDownloadService, 'download');
+      stubDownloadTrack.withArgs(trackOneInfo, match.any).returns(trackOneDlResult);
+      stubDownloadTrack.withArgs(trackTwoInfo, match.any).returns(trackTwoDlResult);
     });
 
     afterEach(() => {
-      stubDownloadTrack$.restore();
+      stubDownloadTrack.restore();
     });
 
-    it('should download each track in playlist', () => {
-      fixture.download$(playlistInfo);
+    it('should download each track in the playlist', () => {
+      fixture.download(playlistInfo);
 
-      expect(stubDownloadTrack$).to.have.been.calledTwice;
-      expect(stubDownloadTrack$.getCall(0).args[0]).to.be.equal(trackOneInfo);
-      expect(stubDownloadTrack$.getCall(1).args[0]).to.be.equal(trackTwoInfo);
+      expect(stubDownloadTrack).to.have.been.calledTwice;
+      expect(stubDownloadTrack.getCall(0).args[0]).to.be.equal(trackOneInfo);
+      expect(stubDownloadTrack.getCall(1).args[0]).to.be.equal(trackTwoInfo);
     });
 
     it('should download to the correct download location', () => {
       const expectedDlLocation = 'some_username_with_special_characters - some_playlist_with_special_characters';
-      fixture.download$(playlistInfo);
+      fixture.download(playlistInfo);
 
-      expect(stubDownloadTrack$).to.have.been.calledTwice;
-      stubDownloadTrack$.getCalls().forEach((spyCall) => expect(spyCall.args[1]).to.be.equal(expectedDlLocation));
+      expect(stubDownloadTrack).to.have.been.calledTwice;
+      stubDownloadTrack.getCalls()
+        .forEach((spyCall) => expect(spyCall.args[1]).to.be.equal(expectedDlLocation));
     });
 
-    it('should return stream of download ids and complete once all tracks have been downloaded', () => {
-      rx.subscribeTo(fixture.download$(playlistInfo));
+    it('should return a playlist download result', () => {
+      const actual = fixture.download(playlistInfo);
 
-      expect(rx.next).to.have.been.calledTwice;
-      expect(rx.next.getCall(0)).to.have.been.calledWithExactly(1);
-      expect(rx.next.getCall(1)).to.have.been.calledWithExactly(2);
-      expect(rx.complete).to.have.been.called;
-    });
-
-    it('should log error and continue downloading other tracks if there is an error downloading a track', () => {
-      stubDownloadTrack$.withArgs(trackOneInfo, match.any).returns(throwError('error downloading track one'));
-      rx.subscribeTo(fixture.download$(playlistInfo));
-
-      expect(rx.error).to.not.have.been.called;
-      expect(rx.next).to.have.been.calledOnce.calledWithExactly(2);
-      expect(rx.complete).to.have.been.called;
+      expect(actual).to.be.deep.equal({
+        kind: ResourceType.Playlist,
+        playlistInfo,
+        tracks: [trackOneDlResult, trackTwoDlResult]
+      });
     });
   });
 });
-*/
--- test/ts/download/user-download-service.spec.ts
+++ test/ts/download/user-download-service.spec.ts
@@ -1,6 +1,6 @@
-/*
-import {ITrackInfo, IUserInfo} from '@src/download/download-info';
-import {DownloadInfoService} from '@src/download/download-info-service';
+import {ITrackDownloadResult} from '@src/download/download-result';
+import {ITrackInfo, IUserInfo, ResourceType} from '@src/download/resource-info';
+import {ResourceInfoService} from '@src/download/resource-info-service';
 import {TrackDownloadService} from '@src/download/track-download-service';
 import {UserDownloadService} from '@src/download/user-download-service';
 import {useFakeTimer, useRxTesting, useSinonChai} from '@test/test-initializers';
@@ -18,75 +18,71 @@ describe('user download service', () => {
   const trackOneInfo: ITrackInfo = {title: 'foo'} as ITrackInfo;
   const trackTwoInfo: ITrackInfo = {title: 'bar'} as ITrackInfo;
   const userInfo: IUserInfo = {
-    kind: 'user',
+    kind: ResourceType.User,
     permalink_url: 'permalinkUrl',
     username: 'some?username/with*special<characters'
   };
   const expectedTrackListInfoUrl = `${userInfo.permalink_url}/tracks`;
+  const trackOneDlResult = {trackInfo: trackOneInfo} as ITrackDownloadResult;
+  const trackTwoDlResult = {trackInfo: trackTwoInfo} as ITrackDownloadResult;
 
   describe('downloading tracks from a user', () => {
     let stubGetTrackInfoList$: SinonStub;
-    let stubDownloadTrack$: SinonStub;
+    let stubDownloadTrack: SinonStub;
 
     beforeEach(() => {
-      stubGetTrackInfoList$ = stub(DownloadInfoService, 'getTrackInfoList$');
+      stubGetTrackInfoList$ = stub(ResourceInfoService, 'getTrackInfoList$');
       stubGetTrackInfoList$.withArgs(expectedTrackListInfoUrl).returns(of([trackOneInfo, trackTwoInfo]));
 
-      stubDownloadTrack$ = stub(TrackDownloadService, 'download$');
-      stubDownloadTrack$.withArgs(trackOneInfo, match.any).returns(of(1));
-      stubDownloadTrack$.withArgs(trackTwoInfo, match.any).returns(of(2));
+      stubDownloadTrack = stub(TrackDownloadService, 'download');
+      stubDownloadTrack.withArgs(trackOneInfo, match.any).returns(trackOneDlResult);
+      stubDownloadTrack.withArgs(trackTwoInfo, match.any).returns(trackTwoDlResult);
     });
 
     afterEach(() => {
       stubGetTrackInfoList$.restore();
-      stubDownloadTrack$.restore();
+      stubDownloadTrack.restore();
     });
 
-    it('should download every track from a user', () => {
+    it('should download every track from the user', () => {
       fixture.download$(userInfo);
 
-      expect(stubDownloadTrack$).to.have.been.calledTwice;
-      expect(stubDownloadTrack$.getCall(0).args[0]).to.be.equal(trackOneInfo);
-      expect(stubDownloadTrack$.getCall(1).args[0]).to.be.equal(trackTwoInfo);
+      expect(stubDownloadTrack).to.have.been.calledTwice;
+      expect(stubDownloadTrack.getCall(0).args[0]).to.be.equal(trackOneInfo);
+      expect(stubDownloadTrack.getCall(1).args[0]).to.be.equal(trackTwoInfo);
     });
 
     it('should download to the correct download location', () => {
       const expectedDlLocation = 'some_username_with_special_characters';
       fixture.download$(userInfo);
 
-      expect(stubDownloadTrack$).to.have.been.calledTwice;
-      stubDownloadTrack$.getCalls().forEach((spyCall) => expect(spyCall.args[1]).to.be.equal(expectedDlLocation));
+      expect(stubDownloadTrack).to.have.been.calledTwice;
+      stubDownloadTrack.getCalls()
+        .forEach((spyCall) => expect(spyCall.args[1]).to.be.equal(expectedDlLocation));
     });
 
-    it('should return stream of download ids and complete once all tracks have been downloaded', () => {
+    it('should emit a user download result and complete if download started successfully', () => {
       rx.subscribeTo(fixture.download$(userInfo));
 
-      expect(rx.next).to.have.been.calledTwice;
-      expect(rx.next.getCall(0)).to.have.been.calledWithExactly(1);
-      expect(rx.next.getCall(1)).to.have.been.calledWithExactly(2);
+      expect(rx.next).to.have.been.calledOnce.calledWithExactly({
+        kind: ResourceType.User,
+        tracks: [trackOneDlResult, trackTwoDlResult],
+        userInfo
+      });
       expect(rx.complete).to.have.been.called;
     });
 
-    it('should log error and continue downloading other tracks if there is an error downloading a track', () => {
-      stubDownloadTrack$.withArgs(trackOneInfo, match.any).returns(throwError('error downloading track one'));
-      rx.subscribeTo(fixture.download$(userInfo));
-
-      expect(rx.error).to.not.have.been.called;
-      expect(rx.next).to.have.been.calledOnce.calledWithExactly(2);
-      expect(rx.complete).to.have.been.called;
-    });
-
-    it('should not return error if fetching track list info takes less than 30 seconds', () => {
+    it('should not emit error if fetching track list info takes less than 30 seconds', () => {
       stubGetTrackInfoList$.withArgs(expectedTrackListInfoUrl)
         .returns(timer(29999).pipe(mapTo([trackOneInfo, trackTwoInfo])));
       rx.subscribeTo(fixture.download$(userInfo));
       cw.clock.tick(30000);
 
       expect(rx.error).to.not.have.been.called;
-      expect(rx.next).to.have.been.calledTwice;
+      expect(rx.next).to.have.been.calledOnce;
     });
 
-    it('should return error if fetching track list info takes 30 seconds or more', () => {
+    it('should emit error if fetching track list info takes 30 seconds or more', () => {
       stubGetTrackInfoList$.withArgs(expectedTrackListInfoUrl)
         .returns(timer(30000).pipe(mapTo([trackOneInfo, trackTwoInfo])));
       rx.subscribeTo(fixture.download$(userInfo));
@@ -97,7 +93,8 @@ describe('user download service', () => {
     });
 
     it('should return error if there is an error fetching track list info', () => {
-      stubGetTrackInfoList$.withArgs(expectedTrackListInfoUrl).returns(throwError('error fetching track list info'));
+      const err = new Error('error fetching track list info');
+      stubGetTrackInfoList$.withArgs(expectedTrackListInfoUrl).returns(throwError(err));
       rx.subscribeTo(fixture.download$(userInfo));
 
       expect(rx.error).to.have.been.called;
@@ -105,4 +102,3 @@ describe('user download service', () => {
     });
   });
 });
-*/
