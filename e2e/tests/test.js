369d003fa16de4af7309fa0b7d4a5f2fc5c9a5fc
xtangle <jackyx5724@hotmail.com>
2018-10-13 18:30:03 -0400

    fix(e2e): Headless chrome doesn't support extensions :(

--- e2e/tests/test.js
+++ e2e/tests/test.js
@@ -1,10 +1,10 @@
 module.exports = {
   'Loads Download button on SoundCloud song page': function(browser) {
     browser
-      .url('https://soundcloud.com')
       .url('https://soundcloud.com/lil-baby-4pf/drip-too-hard')
       .waitForElementVisible('.listenEngagement', 10000)
-      .assert.visible('button.zc-button-download')
+      .verify.visible('button.zc-button-download')
+      .saveScreenshot('e2e/output/song-page-test.png')
       .end();
   }
 };
--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -3,7 +3,7 @@ const extensionPath = path.join(__dirname, 'dist');
 
 module.exports = {
   "src_folders" : ["e2e/tests"],
-  "output_folder" : "e2e/reports",
+  "output_folder" : "e2e/output",
   "page_objects_path" : "",
   "globals_path" : "e2e/globals.js",
   "selenium" : {
@@ -11,8 +11,8 @@ module.exports = {
   },
   "test_settings" : {
     "default" : {
-      "selenium_port"  : 9515,
-      "selenium_host"  : "0.0.0.0",
+      "selenium_port": 9515,
+      "selenium_host": "0.0.0.0",
       "default_path_prefix" : "",
       "request_timeout_options": {
         "timeout": 20000
@@ -22,8 +22,11 @@ module.exports = {
         "browserName": "chrome",
         "chromeOptions" : {
           "args" : [
-            "--headless",
             "--no-sandbox",
+            "--mute-audio",
+            "--disable-gpu",
+            "--window-size=1920,1080",
+            "--start-maximized",
             `--load-extension=${extensionPath}`
           ]
         },
