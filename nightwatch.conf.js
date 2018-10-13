fb88ec454ef20d6fc0fff59858560a4cea0a0666
xtangle <jackyx5724@hotmail.com>
2018-10-13 15:41:13 -0400

    fix(e2e): use ip address instead of localhost

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -12,7 +12,7 @@ module.exports = {
   "test_settings" : {
     "default" : {
       "selenium_port"  : 9515,
-      "selenium_host"  : "localhost",
+      "selenium_host"  : "127.0.0.1",
       "default_path_prefix" : "",
       "request_timeout_options": {
         "timeout": 20000
