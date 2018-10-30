87891aae58849a55f994cb538dc48a80bb1327fc
xtangle <jackyx5724@hotmail.com>
2018-10-29 20:17:36 -0400

    chore: use updated release-tool

--- scripts/pre-release.js
+++ scripts/pre-release.js
@@ -1,10 +1,13 @@
 const JSZip = require('jszip');
 const fs = require('fs');
 const path = require('path');
-const { echo, exec, ls, sed, set } = require('shelljs');
+const { echo, exec, ls, mkdir, rm, sed, set } = require('shelljs');
+
+const manifestPath = path.resolve(__dirname, '../src/resources/manifest.json');
+const distPath = path.resolve(__dirname, '../dist');
+const tmpPath = path.resolve(__dirname, '../tmp');
 
 function updateManifestJson() {
-  const manifestPath = path.resolve(__dirname, '..', 'src', 'resources', 'manifest.json');
   sed('-i', '"version":\\s*"[^"]*"', `"version": "${this.release.newVersion}"`, manifestPath);
   exec(`git add ${manifestPath}`);
   echo('Updated manifest.json');
@@ -13,8 +16,9 @@ function updateManifestJson() {
 async function generateAssets() {
   echo('Generating assets...');
   exec('yarn build:prod', { silent: true });
-  const distPath = path.resolve(__dirname, '..', 'dist');
-  const assetPath = path.resolve(__dirname, '..', `${this.release.name}-${this.release.newTag}.zip`);
+  rm('-rf', tmpPath);
+  mkdir('-p', tmpPath);
+  const assetPath = path.resolve(tmpPath, `${this.release.name}-${this.release.newTag}.zip`);
   await new Promise((resolve) => {
     const zip = new JSZip();
     ls(distPath).forEach(file => zip.file(file, fs.readFileSync(path.resolve(distPath, file))));
@@ -34,7 +38,6 @@ async function generateAssets() {
 module.exports = async function () {
   set('-e');
   updateManifestJson.call(this);
-  const assetPath = await generateAssets.call(this);
+  await generateAssets.call(this);
   set('+e');
-  return assetPath;
 };
