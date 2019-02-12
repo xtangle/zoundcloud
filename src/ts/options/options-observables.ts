258c3c53797a9d2dde978271e4ff5bf8a5f1f1b0
Jacky Xu <jackyxu07@gmail.com>
2019-02-11 23:08:27 -0500

    fix: options observable blocking download

--- src/ts/options/options-observables.ts
+++ src/ts/options/options-observables.ts
@@ -1,9 +1,12 @@
-import {bindCallback} from 'rxjs';
+import {bindCallback, Observable} from 'rxjs';
 import {defaultOptions} from 'src/ts/options/default-options';
 import {IOptions} from 'src/ts/options/option';
 
+// note: cannot use bind on chrome.storage.sync.get function for some reason, have to use a lambda
+const getOptionsWithDefaults = (cb: (result: IOptions) => void) => chrome.storage.sync.get(defaultOptions, cb);
+
 export const OptionsObservables = {
-  getOptions$() {
-    return bindCallback<IOptions, IOptions>(chrome.storage.sync.get)(defaultOptions);
+  getOptions$(): Observable<IOptions> {
+    return bindCallback<IOptions>(getOptionsWithDefaults)();
   },
 };
