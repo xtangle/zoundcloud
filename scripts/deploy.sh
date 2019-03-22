f3c66af57485ea91c7ee5fc1e26bc4979dca751b
Jacky Xu <jackyxu07@gmail.com>
2019-03-21 22:53:45 -0500

    cleanup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,7 +20,9 @@ set -e
 
 echo "Publishing extension to Chrome Web Store..."
 
-ACCESS_TOKEN=$(curl "https://accounts.google.com/o/oauth2/token" -d "client_id=${GOOGLE_CLIENT_ID}&client_secret=${GOOGLE_CLIENT_SECRET}&refresh_token=${GOOGLE_REFRESH_TOKEN}&grant_type=refresh_token&redirect_uri=urn:ietf:wg:oauth:2.0:oob" | jq -r .access_token)
+ACCESS_TOKEN="$( curl "https://accounts.google.com/o/oauth2/token" \
+  -d "client_id=${GOOGLE_CLIENT_ID}&client_secret=${GOOGLE_CLIENT_SECRET}&refresh_token=${GOOGLE_REFRESH_TOKEN}&grant_type=refresh_token&redirect_uri=urn:ietf:wg:oauth:2.0:oob" \
+  | jq -r .access_token )"
 
 curl -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "x-goog-api-version: 2" -X PUT -T tmp/zoundcloud-*.zip -v "https://www.googleapis.com/upload/chromewebstore/v1.1/items/${CHROME_APP_ID}"
 curl -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "x-goog-api-version: 2" -H "Content-Length: 0" -X POST -v "https://www.googleapis.com/chromewebstore/v1.1/items/${CHROME_APP_ID}/publish"
